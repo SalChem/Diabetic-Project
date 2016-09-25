e9ca979051f662630a529fbecfdf3b611e2328ab
Salim Ch <schem001@gmail.com>
2016-09-25 00:54:02 -0400

    First plotting of meal times changes every 5mins

--- Meal Times Adjustment.py
+++ Meal Times Adjustment.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
 ## Matplotlib Variables
-_FIG_SIZE = (16, 12)
+_FIG_SIZE = (6, 4)
 _FIG_FORMAT = 'png'
-_FIG_DPI = 200
+_FIG_DPI = 100
 
 #define the 5 series containing the number of adjusted readings per unit time
 #patient 1-5
@@ -18,9 +18,8 @@ s3 = pd.Series([9,12,13,14,15,16], index=[0,1,2,3,4,5])
 s4 = pd.Series([49,59,68,73,74,75], index=[0,1,2,3,4,5])
 s5 = pd.Series([110,131,136,145,149,156], index=[0,1,2,3,4,5])
 
-x = np.array([i for i in xrange(len(s1))])
-
-plt.figure(figsize=_FIG_SIZE)
+plt.figure()
+#plt.figure(figsize=_FIG_SIZE)
 plt.plot(s1, 'ro', label="Meal Times")
 plt.xlabel('Date')
 plt.legend(loc='upper right')
@@ -28,9 +27,3 @@ plt.title('Glucose Readings with the meal times');
 
 plt.figure()
 s1.plot()
-
-s1.hist()
-
-data = pd.Series(randn(1000))
-
-data.hist(by=randint(0, 4, 1000), figsize=(6, 4))
